4cd60dc65c7341336dca36a4e34bceca7a464a40
Hai Pham <hai.pham@kontist.com>
2021-05-05 15:44:54 +0100

    Make the test description clearer

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,56 +3,6 @@ import expect from "expect";
 import calculateDepreciation from ".";
 
 describe("calculateDepreciation()", () => {
-  it("should handle other months", () => {
-    expect(calculateDepreciation({
-      purchaseAmount: 20000,
-      purchaseDate: new Date("2016-08-01"),
-      totalDepreciationYears: 5,
-    })).toEqual([{
-      year: 2016,
-      depreciationMonths: 5,
-      depreciationAmount: 1666.67,
-      percentage: 8.33,
-      startAmount: 20000,
-      endAmount: 18333.33,
-    }, {
-      year: 2017,
-      depreciationMonths: 12,
-      depreciationAmount: 4000,
-      percentage: 20,
-      startAmount: 18333.33,
-      endAmount: 14333.33,
-    }, {
-      year: 2018,
-      depreciationMonths: 12,
-      depreciationAmount: 4000,
-      percentage: 20,
-      startAmount: 14333.33,
-      endAmount: 10333.33,
-    }, {
-      year: 2019,
-      depreciationMonths: 12,
-      depreciationAmount: 4000,
-      percentage: 20,
-      startAmount: 10333.33,
-      endAmount: 6333.33,
-    }, {
-      year: 2020,
-      depreciationMonths: 12,
-      depreciationAmount: 4000,
-      percentage: 20,
-      startAmount: 6333.33,
-      endAmount: 2333.33,
-    }, {
-      year: 2021,
-      depreciationMonths: 7,
-      depreciationAmount: 2333.33,
-      percentage: 11.67,
-      startAmount: 2333.33,
-      endAmount: 0,
-    }])
-  });
-
   it("should handle January", () => {
     expect(calculateDepreciation({
       purchaseAmount: 20000,
@@ -142,6 +92,56 @@ describe("calculateDepreciation()", () => {
       depreciationAmount: 3666.67,
       percentage: 18.33,
       startAmount: 3666.67,
+      endAmount: 0,
+    }])
+  });
+
+  it("should handle any month, besides January or December", () => {
+    expect(calculateDepreciation({
+      purchaseAmount: 20000,
+      purchaseDate: new Date("2016-08-01"),
+      totalDepreciationYears: 5,
+    })).toEqual([{
+      year: 2016,
+      depreciationMonths: 5,
+      depreciationAmount: 1666.67,
+      percentage: 8.33,
+      startAmount: 20000,
+      endAmount: 18333.33,
+    }, {
+      year: 2017,
+      depreciationMonths: 12,
+      depreciationAmount: 4000,
+      percentage: 20,
+      startAmount: 18333.33,
+      endAmount: 14333.33,
+    }, {
+      year: 2018,
+      depreciationMonths: 12,
+      depreciationAmount: 4000,
+      percentage: 20,
+      startAmount: 14333.33,
+      endAmount: 10333.33,
+    }, {
+      year: 2019,
+      depreciationMonths: 12,
+      depreciationAmount: 4000,
+      percentage: 20,
+      startAmount: 10333.33,
+      endAmount: 6333.33,
+    }, {
+      year: 2020,
+      depreciationMonths: 12,
+      depreciationAmount: 4000,
+      percentage: 20,
+      startAmount: 6333.33,
+      endAmount: 2333.33,
+    }, {
+      year: 2021,
+      depreciationMonths: 7,
+      depreciationAmount: 2333.33,
+      percentage: 11.67,
+      startAmount: 2333.33,
       endAmount: 0,
     }])
   });
